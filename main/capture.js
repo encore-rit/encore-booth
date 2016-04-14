a0f5b6f02b75a3b1fc3243322519bbc31e56cff9
Will <whp3652@rit.edu>
2016-04-14 01:05:37 -0400

    Add capture functionality, babel defaults, and helper lib

---
+++ main/capture.js
@@ -0,0 +1,21 @@
+const { exec } = require('child_process');
+
+function promiseFromChildProcess(child) {
+    return new Promise(function (resolve, reject) {
+        child.addListener("error", reject);
+        child.addListener("exit", resolve);
+    });
+}
+
+/**
+ * @param {String} path path to where to store photo
+ * @returns {Promise<Int>} resolves to 0 or rejects
+ */
+module.exports = function capture(path) {
+  // Dangerously kill all PTP processes in an effort to free up the camera
+  // Then take the picture
+  const CAPTURE_COMMAND =
+    `pkill PTP ; gphoto2 --capture-image-and-download --filename ${path}`;
+
+  return promiseFromChildProcess(exec(CAPTURE_COMMAND));
+};
